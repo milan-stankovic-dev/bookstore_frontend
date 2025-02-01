fed22297601e8391c561d7b1a0235d7ffd0896ab
milan-stankovic-dev <milance.za019@gmail.com>
2025-02-01 12:40:11 +0100

    Added popup for ordering books.

--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -1,13 +1,24 @@
 import { Component, input } from '@angular/core';
 import { BookFull } from '../../domain/book/bookFull';
 // import { BookFull } from '../../domain/book';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-book',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './book.component.html',
   styleUrl: './book.component.scss'
 })
 export class BookComponent {
-  book = input.required<BookFull>()
+
+logValue() {
+ console.log("VALUE: ", this.orderAmount)
+}
+  book = input.required<BookFull>();
+  orderAmount : number = 0
+
+orderAttempt() {
+  
+}
+
 }
